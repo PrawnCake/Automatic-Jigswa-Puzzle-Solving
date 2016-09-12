bd6b7c22a21445cd6905ff8a353b388f894d8ce8
Graeme Faul <graemefaul@gmail.com>
2016-09-12 11:22:11 +0200

    Source setup

---
+++ Automatic Jigsaw puzzle solving/Source.cpp
@@ -0,0 +1,13 @@
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <list>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+int main()
+{
+	return 0;
+}
