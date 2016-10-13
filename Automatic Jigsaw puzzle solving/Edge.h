060553a9fad0efbd02b70430e072bd3647cca610
Graeme Faul <graemefaul@gmail.com>
2016-10-13 18:55:08 +0200

    Neatened header files

--- Automatic Jigsaw puzzle solving/Edge.h
+++ Automatic Jigsaw puzzle solving/Edge.h
@@ -17,22 +17,23 @@ enum EdgeType { STRAIGHT = 0, CONVEX = 1, CONCAVE = 2 };
 class Edge
 {
 public:
-	Edge(vector<Point> e, vector<Point> eActual, Mat img);
+	//functions
 	Edge();
-	void calcChangeInAngles();
-	void calcLengthBetweenPoints();
-
-	void determineEdgeType();
-	vector<double> changeInAngles;
-	vector<double> lengthBetweenPoints;
-	void computeEdgeStrip(Mat img);
-
-
-	EdgeType edgetype;
-	vector<Point> edgePoints;
-	vector<Point> actualEdgePoints;
-	Mat edgeStrip;
 	~Edge();
+	Edge(vector<Point> e, vector<Point> eActual, Mat img);	//Constructor, takes in the poly approximation and actual boundary of the edge 
+	
+	void calcChangeInAngles();			//Traverses the poly approximation of the edge and records the change in angle between approximation points
+	void calcLengthBetweenPoints();		//Traverses the poly approximation of the edge and records the distances between approximation points
+	void determineEdgeType();			//Identifies the type of edge
+	void computeEdgeStrip(Mat img);		//Generates an image comprised of the average RGB value of several pixel deep at each point along the actual contour
 
+	//attributes
+	vector<double> changeInAngles;		//Vector array to store the change in between approximation points an edge
+	vector<double> lengthBetweenPoints;	//Vector array to store the distances between approximation points an edge
+	vector<Point> edgePoints;			//Vector array of each approximation point
+	vector<Point> actualEdgePoints;		//Vector array of every point along the edge boundary
+	EdgeType edgetype;					//Stores the type of edge (straight, convex, concave)
+	Mat edgeStrip;						//Stores the sampled image strip from the computeEdgeStrip function
+	
 };
 
--- Automatic Jigsaw puzzle solving/Piece.h
+++ Automatic Jigsaw puzzle solving/Piece.h
@@ -6,31 +6,29 @@ enum PieceType { CORNER = 0, FRAME = 1, INTERIOR = 2 };
 class Piece
 {
 public:
-	Piece(Mat img, int id);
-	void addEdge(Edge e);
+	//Functions
+	Piece();
+	~Piece();
+	Piece(Mat img, int id);		//Constructor
+	void determinePieceType();	//Counts the number of straight edges to determine the type of piece
+	void createEdges(vector<Point> corners, vector<Point> contours);	//Breaks the overall contour up into its four edges 
+	void addEdge(Edge e);		//Instantiates an Edge object once it has been captured
 
-	void createEdges(vector<Point> corners, vector<Point> contours);
+	//Attributes
+	vector<Point> fullContour;	//Vector array of every Point along the contour
+	Mat image;					//The original input image
+	Point centroid;				//Centre point of the piece
+	PieceType type;				//Piece type
+	bool isInitialised = true;	//Used to keep track of Piece objects that are fully instantiated
 
-	int pieceID;
-	vector<Point> fullContour;
-	bool isInitialised = true;
-	
-	Edge left;
+	Edge * edges = new Edge[4];	//Array of the four edges that make up the piece
+	Edge left;					//Used to classify edges based on their orientation relative to the final solution grid after placement
 	Edge right;
 	Edge up;
 	Edge down;
+	int edgesInitialised = 0;	//Used to keep track of the number of initialised Edge objects
+	int straightEdgeCount = 0;	//Keeps track of the number of straight edges
+
 	
-	Mat image;
-	void determinePieceType();
-	Edge * edges = new Edge[4];
-	Point centroid;
-	PieceType type;
-	int edgesInitialised = 0;
-	int straightEdgeCount = 0;
-
-	Piece();
-	~Piece();
-
-
 };
 
